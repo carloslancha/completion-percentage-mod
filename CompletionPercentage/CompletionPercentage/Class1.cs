72aedfe7061a65e4e20a6ee6c1d931bbd2d09f33
Carlos Lancha <carlitoslancha@gmail.com>
2021-11-01 17:33:19 +0100

    Create Mod

---
+++ CompletionPercentage/CompletionPercentage/Class1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Modding;
+
+namespace CompletionPercentage
+{
+    public class CompletionPercentage: Mod
+    {
+        private float _completionPercentage = 0;
+        private const string _version = "1.0.0";
+        
+        public override string GetVersion()
+        {
+            return _version;
+        }
+        
+        public override void Initialize()
+        {
+            ModHooks.Instance.SetPlayerBoolHook += PlayerBoolSet;
+        }
+        
+        public void PlayerBoolSet(string target, bool value)
+        {
+            PlayerData.instance.SetBoolInternal(target, value);
+
+            if (PlayerData.instance.completionPercentage != _completionPercentage)
+            {
+                _completionPercentage = PlayerData.instance.completionPercentage;
+                WriteHtmlDataFile();
+            }
+        }
+        
+        public void WriteHtmlDataFile()
+        {
+            string path = Directory.GetCurrentDirectory() + @"\hollow_knight_Data\Managed\Mods\CompletionPercentage";
+            string fileName = "data.html";
+            string fullPath = path + "\\" + fileName;
+            string timeString = DateTime.Now.ToString("hh:mm:ss");
+
+            // Create folder if doesn't exist 
+            System.IO.Directory.CreateDirectory(path);
+
+            try    
+            {    
+                // Check if file already exists. If yes, delete it.     
+                if (File.Exists(fullPath))    
+                {    
+                    File.Delete(fullPath);    
+                }    
+    
+                // Create a new file     
+                using (FileStream fs = File.Create(fullPath))
+                {
+                    string html = $"<html><body><script>" +
+                                  $"const data = {{" +
+                                  $"completionPercentage: {_completionPercentage}," +
+                                  $"lastUpdateTime: '{timeString}'" +
+                                  $"}};" +
+                                  $"parent.postMessage(data, \"*\");" +
+                                  $"</script></body></html>";
+                    
+                    fs.Write(new UTF8Encoding(true).GetBytes(html), 0, html.Length);
+                }
+            }    
+            catch (Exception Ex)    
+            {    
+                Log(Ex.ToString());    
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,26 @@
+# completion-percentage-mod
+
+This mod adds support to create an overlay that shows the current completion percentage in Hollow Knight that can be used in OBS for streaming.
+
+# Requirements
+- Hollow Knight 1.4.3.2
+- Modding API 1.4.3.2-60
+
+## How to install
+
+Unzip the [last release](https://github.com/carloslancha/completion-percentage-mod/releases/latest) .zip file in `HollowKnightPath/hollow_knight_Data/Managed/Mods` folder.
+
+## How to integrate in OBS
+
+- Add a new Browser source in your Scene.
+- Select Local File
+- Browse file `HollowKnightPath/hollow_knight_Data/Managed/Mods/CompletionPercentage/overlay.html`
+- Enjoy
+
+## How to customize the Overlay
+- You can customize the position, size and other styles by editing the [styles.css](https://github.com/carloslancha/completion-percentage-mod/blob/master/CompletionPercentage/CompletionPercentage/Resources/styles.css) file.
+
+## License
+
+GNU GPLv3 License © Carlos Lancha.<br/>
+www.twitch.tv/empaventuras
